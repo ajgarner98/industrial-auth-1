5ba03be81b0aae9ffd1a6c701a84d1e97a9bb7b4
Andre Garner <111984130+ajgarner98@users.noreply.github.com>
2023-10-26 21:20:07 +0000

    Finished

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :comments
   resources :follow_requests, except: [:index, :show, :new, :edit, :create]
   resources :likes, only: [:create, :destroy]
-  resources :photos, only: [:index]
+  resources :photos, except: [:index]
 
   get ":username" => "users#show", as: :user
   get ":username/liked" => "users#liked", as: :liked
